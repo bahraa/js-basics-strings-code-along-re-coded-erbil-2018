0d32497fe3c71a6fa0d8c8f3d66009dfac019d26
bahraa <bbahra98@gmail.com>
2018-09-23 12:22:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser='Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, '+ currentUser+ 'currentUse!' ;
+
+const excitedWelcomeMessage='WELCOME TO FLATBOOK, GRACE HOPPER GRACE HOPPER '+ '!';
+
+shortGreeting='Welcome, G !';
